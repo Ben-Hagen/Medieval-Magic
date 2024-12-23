3822f597f91d167f61f3d8822234e06efa66c7cd
Ben-Hagen <spiketendyke@gmail.com>
2024-12-23 14:17:33 +0100

    test commit

---
+++ models/character.class.js
@@ -0,0 +1,10 @@
+class Character extends MoveableObject {
+
+    constructor() {
+        super().loadImage("Mediaval-Magic/img/character/Fire vizard/Walk.png");
+    }
+
+    jump() {
+
+    }
+}
---
+++ models/slime.class.js
@@ -0,0 +1,6 @@
+class Slime extends MoveableObject {
+    constructor() {
+        super().loadImage("../img/enemies/Skeleton/Idle.png");
+    }
+
+}
